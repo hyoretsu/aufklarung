52a893f2277bec16b2379632e6a76b89b1aeb524
Aran Leite <hyoretsu@gmail.com>
2020-06-01 21:22:43 -0300

    Layout básico do header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,57 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiChevronDown, FiSearch } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.png';
 
 import { Container } from './styles';
 
 const Header: React.FC = () => {
- return <Container></Container>;
+ return (
+  <Container>
+   <div>
+    <span>Acesso</span>
+   </div>
+   <Link to="/">
+    <img src={logoImg} alt="Aufklärung - Revista de Filosofia" />
+   </Link>
+   <nav>
+    <Link to="/">Início</Link>
+    <Link to="/current">Atual</Link>
+    <Link to="/archives">Arquivos</Link>
+    <Link to="/news">Notícias</Link>
+    <label>
+     Sobre
+     <FiChevronDown />
+     <ul>
+      <li>
+       <Link to="/about">Sobre a revista</Link>
+      </li>
+      <li>
+       <Link to="/editorial_team">Equipe Editorial</Link>
+      </li>
+      <li>
+       <Link to="/ficha">Ficha Catalográfica</Link>
+      </li>
+      <li>
+       <Link to="/contact">Contato</Link>
+      </li>
+      <li>
+       <Link to="/submissions">Submissões</Link>
+      </li>
+      <li>
+       <Link to="/privacy">Declaração de Privacidade</Link>
+      </li>
+     </ul>
+    </label>
+    <Link to="/info">Informações</Link>
+   </nav>
+   <button>
+    <FiSearch />
+    Buscar
+   </button>
+  </Container>
+ );
 };
 
 export default Header;
