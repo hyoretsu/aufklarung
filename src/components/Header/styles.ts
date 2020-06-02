200dd48312c4d6c9d763c4cb1a8ab04b3969834b
Aran Leite <hyoretsu@gmail.com>
2020-06-01 23:36:52 -0300

    Estilização da seção de usuário

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,3 +1,24 @@
 import styled from 'styled-components';
 
-export const Container = styled.header``;
+export const Container = styled.header`
+ background: #e7e7e7;
+
+ div {
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  height: 2vw;
+  border-bottom: 1px solid #ff7f00;
+
+  a {
+   color: #ff7f00;
+   font: small-caps 600 0.8em 'Segoe UI';
+   margin-right: 2vw;
+
+   &:hover,
+   &:focus {
+    color: #007ab2;
+   }
+  }
+ }
+`;
