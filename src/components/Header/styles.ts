d44dcc07a483e66b3128abb94d910fa780c06b91
Aran Leite <hyoretsu@gmail.com>
2020-06-06 17:32:13 -0300

    Estilização do botão de busca

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -2,13 +2,14 @@ import styled from 'styled-components';
 
 export const Container = styled.header`
  background: #e7e7e7;
+ width: auto;
 
  div {
   display: flex;
   align-items: center;
   justify-content: flex-end;
   height: 2vw;
-  border-bottom: 1px solid #ff7f00;
+  border-bottom: 0.1vh solid #ff7f00;
 
   a {
    color: #ff7f00;
@@ -22,15 +23,58 @@ export const Container = styled.header`
   }
  }
 
- a {
+ > a {
   cursor: default;
 
   img {
-   height: 7.7vw;
+   height: 7.2vw;
    display: block;
-   padding-top: 0.5vw;
-   margin: 0 auto;
+   margin: 0.7vw auto 1vw;
    cursor: pointer;
+  }
+ }
+`;
+
+export const Navigation = styled.nav`
+ display: flex;
+
+ label {
+  cursor: pointer;
+
+  ul {
+   display: none;
+   list-style: none;
+  }
+
+  &:hover ul {
+   display: block;
+  }
+ }
+
+ button {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  font: small-caps 600 1em 'Segoe UI';
+  color: #ff7f00;
+  margin-left: auto;
+  padding: 1.5vh 1vw 1vh;
+  box-sizing: border-box;
+  max-height: 100%;
+
+  background: transparent;
+  border: none;
+
+  svg {
+   margin-right: 0.5vw;
+   height: 2.4vh;
+   width: auto;
+  }
+
+  &:hover {
+   color: #007ab2;
+   padding-bottom: 0.6vh;
+   border-bottom: 0.4vh solid #007ab2;
   }
  }
 `;
--- src/components/Root/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import { Container } from './styles';
-
-const Root: React.FC = () => {
- return <Container></Container>;
-};
-
-export default Root;
