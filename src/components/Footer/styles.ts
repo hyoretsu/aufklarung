700f2e59d1da01df985182b6f2bb9ab8600bf935
Aran Leite <hyoretsu@gmail.com>
2020-06-08 01:26:38 -0300

    Footer text added

--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -1,3 +1,12 @@
 import styled from 'styled-components';
 
-export const Container = styled.footer``;
+export const Container = styled.footer`
+ display: flex;
+ flex-direction: column;
+ align-items: center;
+ justify-content: center;
+ background: #ddd;
+
+ font-size: 0.8em;
+ font-family: 'Segoe UI';
+`;
