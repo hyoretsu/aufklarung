3a516e04604cb779e0f99f8066bdde46a4881dfe
Aran Leite <hyoretsu@gmail.com>
2020-06-08 16:28:19 -0300

    Footer icons core added

---
+++ src/components/Footer/components/FooterIcons/index.tsx
@@ -0,0 +1,239 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const FooterIcons: React.FC = () => {
+ return (
+  <Container>
+   <a href="https://www.ebsco.com/" title="EBSCO Information Services" target="_blank" rel="noopener noreferrer">
+    <img src="" alt="EBSCO Information Services" />
+   </a>
+   <a href="http://www.wokinfo.com/" title="Web of Science" target="_blank" rel="noopener noreferrer">
+    <img src="" alt="Web of Science"></img>
+   </a>
+   <a
+    href="https://www.ebscohost.com/titleLists/pft-coverage.htm"
+    title="Philosopher's Index"
+    target="_blank"
+    rel="noopener noreferrer"
+   >
+    <img src="" alt="The Philosopher's Index" />
+   </a>
+   <a href="https://www.periodicos.capes.gov.br/" title="Periódicos CAPES" target="_blank" rel="noopener noreferrer">
+    <img src="" alt="Portal de Periódicos CAPES/MEC" />
+   </a>
+   <a
+    href="https://periodicos.ufpb.br/ojs2/index.php"
+    title="Portal de Periódicos Científicos da UFPB"
+    target="_blank"
+    rel="noopener noreferrer"
+   >
+    <img src="" alt="Portal de Periódicos Científicos da UFPB" />
+   </a>
+   <a
+    href="http://cariniana.ibict.br/"
+    title="Rede Cariniana - Instituto Brasileiro de Informação em Ciência e Tecnologia"
+    target="_blank"
+    rel="noopener noreferrer"
+   >
+    <img src="" alt="Rede Cariniana" />
+   </a>
+   <a href="https://doaj.org/" title="Directory of Open Access Journals" target="_blank" rel="noopener noreferrer">
+    <img src="" alt="DOAJ" />
+   </a>
+   <a
+    href="https://diadorim.ibict.br/"
+    title="Diretório de políticas editoriais das revistas científicas brasileiras"
+    target="_blank"
+    rel="noopener noreferrer"
+   >
+    <img src="" alt="DIADORIM" />
+   </a>
+   <a
+    href="http://oasisbr.ibict.br/vufind/"
+    title="Portal brasileiro de publicações científicas em acesso aberto"
+    target="_blank"
+    rel="noopener noreferrer"
+   >
+    <img src="" alt="oasisbr" />
+   </a>
+   <a
+    href="https://www.abecbrasil.org.br/novo/"
+    title="Associação Brasileira de Editores Científicos"
+    target="_blank"
+    rel="noopener noreferrer"
+   >
+    <img src="" alt="ABEC Brasil" />
+   </a>
+   <a
+    href="http://www.anpof.org/portal/"
+    title="Associação Nacional de Pós-Graduação em Filosofia"
+    target="_blank"
+    rel="noopener noreferrer"
+   >
+    <img src="" alt="ANPOF" />
+   </a>
+   <a
+    href="http://www.sherpa.ac.uk/romeo/index.php"
+    title="Publisher copyright policies & self-archiving"
+    target="_blank"
+    rel="noopener noreferrer"
+   >
+    <img src="" alt="SHERPA/RoMEO" />
+   </a>
+   <a href="https://clockss.org/" title="Controlled LOCKSS" target="_blank" rel="noopener noreferrer">
+    <img src="" alt="CLOCKSS" />
+   </a>
+   <a
+    href="http://www.openarchives.org/Register/BrowseSites"
+    title="OAI-PMH Registered Data Providers"
+    target="_blank"
+    rel="noopener noreferrer"
+   >
+    <img src="" alt="Open Archives Initiative" />
+   </a>
+   <a
+    href="https://www.latindex.org/latindex/inicio"
+    title="Catálogo Latindex"
+    target="_blank"
+    rel="noopener noreferrer"
+   >
+    <img src="" alt="Catálogo Latindex" />
+   </a>
+   <a
+    href="http://clase.unam.mx/"
+    title="Citas Latinoamericanas en Ciencias Sociales y Humanidades"
+    target="_blank"
+    rel="noopener noreferrer"
+   >
+    <img src="" alt="CLASE" />
+   </a>
+   <a
+    href="https://biblat.unam.mx/"
+    title="Bibliografia Latinoamericana en revistas de investigación científica y social"
+    target="_blank"
+    rel="noopener noreferrer"
+   >
+    <img src="" alt="Biblat" />
+   </a>
+   <a
+    href="http://dialnet.unirioja.es/"
+    title="Fundación Dialnet - Universidad de la Rioja"
+    target="_blank"
+    rel="noopener noreferrer"
+   >
+    <img src="" alt="DIALNET" />
+   </a>
+   <a
+    href="https://dbh.nsd.uib.no/publiseringskanaler/erihplus/index"
+    title="European Reference Index for the Humanities and Social Sciences"
+    target="_blank"
+    rel="noopener noreferrer"
+   >
+    <img src="" alt="ERIH PLUS" />
+   </a>
+   <a
+    href="http://www.cnen.gov.br/centro-de-informacoes-nucleares/livre"
+    title="Periódicos de livre acesso"
+    target="_blank"
+    rel="noopener noreferrer"
+   >
+    <img src="" alt="LivRe" />
+   </a>
+   <a
+    href="http://www.opensciencedirectory.net/"
+    title="Open Science Directory"
+    target="_blank"
+    rel="noopener noreferrer"
+   >
+    <img src="" alt="Open Science Directory" />
+   </a>
+   <a
+    href="https://www.redib.org/"
+    title="Red Iberoamericana de Innovación y Conocimiento Cientófico"
+    target="_blank"
+    rel="noopener noreferrer"
+   >
+    <img src="" alt="REDIB" />
+   </a>
+   <a
+    href="http://www.sumarios.org/"
+    title="Sumários de Revistas Brasileiras"
+    target="_blank"
+    rel="noopener noreferrer"
+   >
+    <img src="" alt="Sumários.org" />
+   </a>
+   <a
+    href="https://scholar.google.com.br/citations?user=rzLghK8AAAAJ"
+    title="Google Scholar"
+    target="_blank"
+    rel="noopener noreferrer"
+   >
+    <img src="" alt="Google Scholar" />
+   </a>
+   <a
+    href="http://oaji.net/journal-detail.html?number=1381"
+    title="Open Academic Journal Index"
+    target="_blank"
+    rel="noopener noreferrer"
+   >
+    <img src="" alt="OAJI.net" />
+   </a>
+   <a
+    href="https://base-search.net/Search/Results?lookfor=etd"
+    title="Bielefeld Academic Search Engine"
+    target="_blank"
+    rel="noopener noreferrer"
+   >
+    <img src="" alt="Bielefeld Academic Search Engine"></img>
+   </a>
+   <a
+    href="https://discover.libraryhub.jisc.ac.uk/"
+    title="Jisc Library Hub Discover"
+    target="_blank"
+    rel="noopener noreferrer"
+   >
+    <img src="" alt="Jisc" />
+   </a>
+   <a
+    href="https://rebiun.baratz.es/rebiun/"
+    title="Red de Biblioteca Universitarias Españolas"
+    target="_blank"
+    rel="noopener noreferrer"
+   >
+    <img src="" alt="Catálogo REBIUN" />
+   </a>
+   <a
+    href="http://www.cnen.gov.br/centro-de-informacoes-nucleares"
+    title="Centro de Informações Nucleares"
+    target="_blank"
+    rel="noopener noreferrer"
+   >
+    <img src="" alt="CNEN" />
+   </a>
+   <a
+    href="http://lib.utep.edu/search/tAufforderung+zum+Tanze./taufforderung+zum+tanze/-3,-1,0,B/browse"
+    title="The University of Texas at El Paso Library"
+    target="_blank"
+    rel="noopener noreferrer"
+   >
+    <img src="" alt="UTEP" />
+   </a>
+   <a href="https://biblio.uottawa.ca/" title="University of Ottawa" target="_blank" rel="noopener noreferrer">
+    <img src="" alt="uOttawa" />
+   </a>
+   <a href="https://www.worldcat.org/" title="WorldCat" target="_blank" rel="noopener noreferrer">
+    <img src="" alt="WorldCat" />
+   </a>
+   <a href="https://worldwidescience.org/" title="WorldWideScience" target="_blank" rel="noopener noreferrer">
+    <img src="" alt="WorldWideScience" />
+   </a>
+   <a href="https://www.crossref.org/" title="Crossref" target="_blank" rel="noopener noreferrer">
+    <img src="" alt="Crossref" />
+   </a>
+  </Container>
+ );
+};
+
+export default FooterIcons;
