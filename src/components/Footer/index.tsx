294a669bccab78d2760a7988506fa2834186aea1
Aran Leite <hyoretsu@gmail.com>
2020-06-08 01:26:38 -0300

    Footer text added

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,7 +3,16 @@ import React from 'react';
 import { Container } from './styles';
 
 const Footer: React.FC = () => {
- return <Container></Container>;
+ return (
+  <Container>
+   <strong>Registros e Indexações desta Revista:</strong>
+   <p>
+    © 2014 Aufklärung<span>®</span>, doi:10.18012/ARF, e-ISSN 2318-9428, p-ISSN 2358-8470 |{' '}
+    <a href="mailto:revistaaufklarung@hotmail.com">revistaaufklarung@hotmail.com</a> | João Pessoa - PB, Brasil |{' '}
+    <a href="https://creativecommons.org/licenses/by/4.0/">CC BY Attribution 4.0 International</a>
+   </p>
+  </Container>
+ );
 };
 
 export default Footer;
