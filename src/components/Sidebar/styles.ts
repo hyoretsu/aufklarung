4bc409fcfaf9f1532845dcbe72cf017c843406dd
Aran Leite <hyoretsu@gmail.com>
2020-06-11 19:58:29 -0300

    Basic article layout

--- src/components/Sidebar/styles.ts
+++ src/components/Sidebar/styles.ts
@@ -4,6 +4,7 @@ export const Container = styled.aside`
  display: flex;
  flex-direction: column;
  width: 16vw;
+ margin-left: 2vw;
 `;
 
 export const SubmissionButton = styled.button``;
--- src/pages/Homepage/styles.ts
+++ src/pages/Homepage/styles.ts
@@ -4,4 +4,5 @@ export const Content = styled.div`
  display: flex;
  height: 300px;
  background: #f5f5f5;
+ margin: 4vh 1vw;
 `;
