bf50976efdf59143ea69ba565a29544eda191746
Aran Leite <hyoretsu@gmail.com>
2020-06-11 19:42:36 -0300

    Basic sidebar layout

--- src/components/Sidebar/styles.ts
+++ src/components/Sidebar/styles.ts
@@ -1,5 +1,9 @@
 import styled from 'styled-components';
 
 export const Container = styled.aside`
+ display: flex;
+ flex-direction: column;
  width: 16vw;
 `;
+
+export const SubmissionButton = styled.button``;
