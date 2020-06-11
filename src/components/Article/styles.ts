491dc4010e43bf8357a02b16370c57893ca8d697
Aran Leite <hyoretsu@gmail.com>
2020-06-11 19:39:34 -0300

    Basic body layout

---
+++ src/components/Article/styles.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const Container = styled.main`
+ flex: 1;
+ background: #fff;
+`;
--- src/components/Sidebar/styles.ts
+++ src/components/Sidebar/styles.ts
@@ -1,3 +1,5 @@
 import styled from 'styled-components';
 
-export const Container = styled.aside``;
+export const Container = styled.aside`
+ width: 16vw;
+`;
