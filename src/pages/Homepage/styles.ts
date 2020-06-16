b5db3ceb0fe976df9529377ac3d156b6819c8a90
Aran Leite <hyoretsu@gmail.com>
2020-06-16 02:41:48 -0300

    HomepageNews styling

--- src/pages/Homepage/styles.ts
+++ src/pages/Homepage/styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { transparentize } from 'polished';
 
 export const Content = styled.div`
  display: flex;
@@ -10,4 +11,73 @@ export const Content = styled.div`
  }
 `;
 
-export const News = styled.div``;
+export const HomepageNews = styled.div`
+ display: flex;
+ flex-wrap: wrap;
+ border-bottom: 1px solid #ddd;
+
+ section {
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+
+  padding: 2vh 2vw;
+  font: 0.9em 'Segoe UI';
+
+  div {
+   display: flex;
+   align-items: center;
+   color: ${transparentize(0.5, '#000')};
+
+   margin: 10px 0;
+
+   svg {
+    height: 18px;
+    width: auto;
+    color: #ddd;
+
+    margin-right: 2%;
+   }
+  }
+
+  a {
+   display: flex;
+   align-items: center;
+   font-weight: 700;
+   color: #ff7f00;
+   margin-right: auto;
+   max-width: 90%;
+
+   strong {
+    font-variant: small-caps;
+    font-size: 1.2em;
+   }
+
+   svg {
+    margin: 5px;
+   }
+
+   &:hover {
+    color: #65a6c2;
+   }
+  }
+
+  p + a {
+   margin-top: auto;
+  }
+
+  & + section {
+   max-width: 370px;
+   border-left: 1px solid #ddd;
+
+   div {
+    margin: auto 0;
+   }
+
+   @media (max-width: 426px) {
+    border-left: none;
+    border-top: 1px solid #ddd;
+   }
+  }
+ }
+`;
