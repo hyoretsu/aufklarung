c5067dd6e4faad20e6f052864e04ea0d0d67f7d0
Aran Leite <hyoretsu@gmail.com>
2020-06-19 01:50:16 -0300

    Issue's Section styling

--- src/components/Issue/index.tsx
+++ src/components/Issue/index.tsx
@@ -20,20 +20,27 @@ const Issue: React.FC = () => {
     </span>
    </Introduction>
    <strong>Editorial</strong>
-   <Section>
-    <Link to="/issue/25/articles/1">Apresentação</Link>
-    <p>Claudinei Aparecido de Freitas da Silva</p>
-    <span>p.7-10</span>
-    <button type="button">
-     <FaRegFilePdf />
-     PDF
-    </button>
-   </Section>
+   <div>
+    <Section>
+     <Link to="/issue/25/articles/1">Apresentação</Link>
+     <div>
+      <p>Claudinei Aparecido de Freitas da Silva</p>
+      <span>p.7-10</span>
+     </div>
+
+     <button type="button">
+      <FaRegFilePdf />
+      PDF
+     </button>
+    </Section>
+   </div>
    <strong>Artigos</strong>
    <Section>
     <Link to="/issue/25/articles/2">Revolução estética - a livre invenção de espaços de liberdade</Link>
-    <p>Luciano Donizetti da Silva</p>
-    <span>p.11-24</span>
+    <div>
+     <p>Luciano Donizetti da Silva</p>
+     <span>p.11-24</span>
+    </div>
     <button type="button">
      <FaRegFilePdf />
      PDF
@@ -41,8 +48,10 @@ const Issue: React.FC = () => {
    </Section>
    <Section>
     <Link to="/issue/25/articles/3">Filosofia e literatura: em torno de A Náusea</Link>
-    <p>Luiza Helena Hilgert</p>
-    <span>p.25-38</span>
+    <div>
+     <p>Luiza Helena Hilgert</p>
+     <span>p.25-38</span>
+    </div>
     <button type="button">
      <FaRegFilePdf />
      PDF
@@ -50,8 +59,10 @@ const Issue: React.FC = () => {
    </Section>
    <Section>
     <Link to="/issue/25/articles/4">Les Mouches: liberdade situada e reflexividades em Jean-Paul Sartre</Link>
-    <p>Marivania Cristina Bocca, Tiago Soares dos Santos</p>
-    <span>p.39-50</span>
+    <div>
+     <p>Marivania Cristina Bocca, Tiago Soares dos Santos</p>
+     <span>p.39-50</span>
+    </div>
     <button type="button">
      <FaRegFilePdf />
      PDF
@@ -59,8 +70,10 @@ const Issue: React.FC = () => {
    </Section>
    <Section>
     <Link to="/issue/25/articles/5">A experiência do non-sens em Roquentin: A Náusea sob as lentes da psicanálise</Link>
-    <p>Renato Santos</p>
-    <span>p.51-64</span>
+    <div>
+     <p>Renato Santos</p>
+     <span>p.51-64</span>
+    </div>
     <button type="button">
      <FaRegFilePdf />
      PDF
@@ -68,8 +81,10 @@ const Issue: React.FC = () => {
    </Section>
    <Section>
     <Link to="/issue/25/articles/6">O caráter nadificador da consciência imaginante</Link>
-    <p>Lucas Rodrigues da Fonseca Lopes</p>
-    <span>p.65-76</span>
+    <div>
+     <p>Lucas Rodrigues da Fonseca Lopes</p>
+     <span>p.65-76</span>
+    </div>
     <button type="button">
      <FaRegFilePdf />
      PDF
@@ -77,8 +92,10 @@ const Issue: React.FC = () => {
    </Section>
    <Section>
     <Link to="/issue/25/articles/7">A Fenomenologia de Heidegger e Sartre em suas diferenças</Link>
-    <p>Daniela Ribeiro Schneider</p>
-    <span>p.77-92</span>
+    <div>
+     <p>Daniela Ribeiro Schneider</p>
+     <span>p.77-92</span>
+    </div>
     <button type="button">
      <FaRegFilePdf />
      PDF
@@ -88,8 +105,10 @@ const Issue: React.FC = () => {
     <Link to="/issue/25/articles/8">
      Literatura e Humanismo: Fenômenos éticos no confronto entre Sartre e Dostoiévski
     </Link>
-    <p>Cristiane Picinini</p>
-    <span>p.93-102</span>
+    <div>
+     <p>Cristiane Picinini</p>
+     <span>p.93-102</span>
+    </div>
     <button type="button">
      <FaRegFilePdf />
      PDF
@@ -97,8 +116,10 @@ const Issue: React.FC = () => {
    </Section>
    <Section>
     <Link to="/issue/25/articles/9">O fenômeno da angústia: interfaces entre Graciliano Ramos e Sartre</Link>
-    <p>Camila Pacheco Gomes</p>
-    <span>p.103-114</span>
+    <div>
+     <p>Camila Pacheco Gomes</p>
+     <span>p.103-114</span>
+    </div>
     <button type="button">
      <FaRegFilePdf />
      PDF
@@ -108,8 +129,10 @@ const Issue: React.FC = () => {
     <Link to="/issue/25/articles/10">
      A estética do movimento: uma leitura sartriana da psicoterapia a partir da arte de Alberto Giacometti
     </Link>
-    <p>Georges Daniel Janja Bloc Boris, Daniel Marcio Pereira Melo</p>
-    <span>p.115-126</span>
+    <div>
+     <p>Georges Daniel Janja Bloc Boris, Daniel Marcio Pereira Melo</p>
+     <span>p.115-126</span>
+    </div>
     <button type="button">
      <FaRegFilePdf />
      PDF
@@ -119,8 +142,10 @@ const Issue: React.FC = () => {
     <Link to="/issue/25/articles/11">
      El existencialismo de Sartre, origenes y contextos: una lectura a partir de Hannah Arendt
     </Link>
-    <p>Gerardo Miguel Nieves-Loja</p>
-    <span>p.127-136</span>
+    <div>
+     <p>Gerardo Miguel Nieves-Loja</p>
+     <span>p.127-136</span>
+    </div>
     <button type="button">
      <FaRegFilePdf />
      PDF
@@ -128,8 +153,10 @@ const Issue: React.FC = () => {
    </Section>
    <Section>
     <Link to="/issue/25/articles/12">Aportes clínicos sartrianos III: método progressivo-regressivo</Link>
-    <p>Claudinei Aparecido de Freitas da Silva</p>
-    <span>p.137-150</span>
+    <div>
+     <p>Claudinei Aparecido de Freitas da Silva</p>
+     <span>p.137-150</span>
+    </div>
     <button type="button">
      <FaRegFilePdf />
      PDF
@@ -137,8 +164,10 @@ const Issue: React.FC = () => {
    </Section>
    <Section>
     <Link to="/issue/25/articles/13">O tempo em Heidegger e Thomas Mann</Link>
-    <p>Libanio Cardoso</p>
-    <span>p.153-168</span>
+    <div>
+     <p>Libanio Cardoso</p>
+     <span>p.153-168</span>
+    </div>
     <button type="button">
      <FaRegFilePdf />
      PDF
