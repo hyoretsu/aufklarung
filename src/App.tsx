ab94b842fe89ed8dd28458fcbf310c3e6233da7d
Aran Leite <hyoretsu@gmail.com>
2020-06-19 23:21:45 -0300

    Finished scroll retention

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,25 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { Router } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+import { wrapHistory } from 'oaf-react-router';
 
 import GlobalStyle from './styles/global';
 import Fonts from './assets/fonts';
 import Routes from './routes';
-import ScrollToTop from './utils/ScrollToTop';
 
-const App: React.FC = () => (
- <>
-  <BrowserRouter>
-   <ScrollToTop />
-   <Routes />
-  </BrowserRouter>
-  <GlobalStyle />
-  <Fonts />
- </>
-);
+const App: React.FC = () => {
+ const history = createBrowserHistory();
+ wrapHistory(history);
+
+ return (
+  <>
+   <Router history={history}>
+    <Routes />
+   </Router>
+   <GlobalStyle />
+   <Fonts />
+  </>
+ );
+};
 
 export default App;
