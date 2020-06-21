4ecf097bf08142a7a282ee1c8fc05c014e8ede98
Aran Leite <hyoretsu@gmail.com>
2020-06-21 14:00:24 -0300

    Finished Archives page styling
    Also added a bottom margin to LocationInfo

--- src/pages/Archives/styles.ts
+++ src/pages/Archives/styles.ts
@@ -1,3 +1,27 @@
 import styled from 'styled-components';
+import { transparentize } from 'polished';
 
-export const Section = styled.section``;
+export const Section = styled.section`
+ display: flex;
+ padding: 2vh 2vw;
+ border-top: 1px solid #ddd;
+
+ a {
+  img {
+   max-height: 15vw;
+  }
+ }
+
+ div {
+  max-width: 78%;
+  margin-left: 2vw;
+
+  span {
+   color: ${transparentize(0.5, '#000')};
+  }
+
+  p {
+   margin-top: 2vh;
+  }
+ }
+`;
