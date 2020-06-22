cd85b581eb62bba6f7332bb8ffcc34edb2ac0fd1
Aran Leite <hyoretsu@gmail.com>
2020-06-21 21:13:07 -0300

    Refactored Homepage/Info's styling
    Moved page-specific outer level styling, previous on DefaultTemplate's Body component, to each page's own Styling component

--- src/pages/Homepage/styles.ts
+++ src/pages/Homepage/styles.ts
@@ -1,5 +1,19 @@
 import styled from 'styled-components';
 
+export const Styling = styled.div`
+ > h3 {
+  font-size: 1.4em;
+  font-weight: 700;
+  font-variant: small-caps;
+  color: #ff9000;
+  margin: 1vh auto 0 2vw;
+
+  @media (max-width: 426px) {
+   margin-left: 4vw;
+  }
+ }
+`;
+
 export const HomepageNews = styled.div`
  display: flex;
  flex-wrap: wrap;
