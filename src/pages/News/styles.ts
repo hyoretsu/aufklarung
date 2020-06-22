6158174b441d2a5aac8c0dda2b58868042d7a842
Aran Leite <hyoretsu@gmail.com>
2020-06-21 22:02:01 -0300

    Finished News (individual) page styling

--- src/pages/News/styles.ts
+++ src/pages/News/styles.ts
@@ -1,3 +1,37 @@
 import styled from 'styled-components';
+import { transparentize } from 'polished';
 
-export const Styling = styled.div``;
+export const Styling = styled.div`
+ margin: 2vh 2vw 2vh 2vw;
+
+ @media (max-width: 426px) {
+  margin-left: 4vw;
+  margin-bottom: 3vh;
+ }
+
+ > h2 {
+  font-variant: small-caps;
+  color: #ff9000;
+ }
+
+ div {
+  display: flex;
+  align-items: center;
+  color: ${transparentize(0.5, '#000')};
+
+  margin: 10px 0;
+
+  svg {
+   height: 18px;
+   width: auto;
+   color: #ddd;
+
+   margin-right: ${window.location.pathname.includes('/news') ? '1%' : '2%'};
+  }
+ }
+
+ p {
+  font-size: 0.9em;
+  line-height: 1.7;
+ }
+`;
