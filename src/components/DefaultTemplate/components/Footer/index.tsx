a4650f096c70b155a67b66a60325db0646f58403
Aran Leite <hyoretsu@gmail.com>
2020-07-16 22:03:05 -0300

    Moved FooterIcons

--- src/components/DefaultTemplate/components/Footer/index.tsx
+++ src/components/DefaultTemplate/components/Footer/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Icons from './components/FooterIcons';
+import Icons from './FooterIcons';
 import { Container } from './styles';
 
 const Footer: React.FC = () => {
