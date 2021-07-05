fa5aefe27c9f8226926d708508d84007331c1d5d
Aran Leite <hyoretsu@gmail.com>
2021-07-04 21:24:16 -0300

    Updated favicons
    They were the only things that didn't use #ff7700 for orange. Also compressed everything

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -16,12 +16,12 @@ class MyDocument extends Document {
      <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
      <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
      <link rel="icon" type="image/png" sizes="192x192" href="/favicons/android-chrome-192x192.png" />
-     <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#f78f22" />
+     <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#ff7700" />
      <link rel="manifest" href="/site.webmanifest" />
      <meta name="application-name" content={siteName} />
      <meta name="apple-mobile-web-app-title" content={siteName} />
-     <meta name="msapplication-TileColor" content="#f78f22" />
-     <meta name="theme-color" content="#f78f22" />
+     <meta name="msapplication-TileColor" content="#ff7700" />
+     <meta name="theme-color" content="#ff7700" />
     </Head>
     <body>
      <Main />
